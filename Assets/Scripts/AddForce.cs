241551667d0d4d91dd274c3e1217665fd1e74bfe
Create Unity Project <ryohei@R-NakanonoMacBook-Pro.local>
2018-02-11 14:37:31 +0900

    ・画面幅に依らずにボールを飛ばせる実装に変更 ＊【AddForce】のみ

--- Assets/Scripts/AddForce.cs
+++ Assets/Scripts/AddForce.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class AddForce : MonoBehaviour {
 
-	float power = 0.04f;//ボールに加える速度ベクトルがあまりにも大きくなりすぎないように調整
+	float power = 15f;//ボールに加える速度ベクトルがあまりにも大きくなりすぎないように調整
 
 	Rigidbody r;//GetComponentしたRigidbodyを入れておく為の変数
 	Vector3 ballVec;//ボールが飛んで方向
 
 	Vector3 clickPosDown;//マウス押し込んだ位置
 	Vector3 clickPosUp;//マウス離した位置
+	Vector3 clickPosDiff;//clickPosDownとclickPosUpの2点間ベクトル
 	float distance;//2点間の距離
-	float distanceMin = 50;//飛ばす最小値
+	float distanceMin = 0.005f;//飛ばす最小値
 
 
 	// Use this for initialization
@@ -41,7 +42,9 @@ public class AddForce : MonoBehaviour {
 		{
 			clickPosUp = Input.mousePosition;//マウス離した瞬間のマウスの位置座標を変数"clickPosUp"に格納
 
-			distance = (clickPosUp - clickPosDown).magnitude;//2点間距離を求める
+			distance = (clickPosUp - clickPosDown).magnitude / Screen.width;//2点間距離を求める
+			//異なるScreen幅に対応する為にScreen.widthで割る→割合にしている！
+
 			Debug.Log(distance);
 
 			//マウス押し込んだ座標と離した座標があまりにも近すぎる場合→処理抜ける(先に進まない)
